956e938d5bd681fc2c033a653e319b8bb239ccf0
Deepika2730 <96734238+Deepika2730@users.noreply.github.com>
2022-10-10 21:18:58 +0530

    x=int(input()) l=list(map(int,input().split())) c=0 for i in set(l):     if i==l.count(i):         c+=1 print(c)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,7 +1,7 @@
-n=int(input())
-a=list(map(int,input().split()))
+x=int(input())
+l=list(map(int,input().split()))
 c=0
-for i in sorted(set(a),key=a.index):
-    if a.count(i)==i:
+for i in set(l):
+    if i==l.count(i):
         c+=1
 print(c)

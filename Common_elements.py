e2f1bc6b97a64711e912c62e3cfa38bb0c634755
Deepika2730 <96734238+Deepika2730@users.noreply.github.com>
2022-10-12 19:15:58 +0530

    a,b=map(int,input().split()) l = list(map(int,input().split())) m = list(map(int,input().split())) c=0 for i in sorted(set(l),key=l.index):     if m.count(i)!=0:         print(i,end=" ")

--- Common_elements.py
+++ Common_elements.py
@@ -1,7 +1,7 @@
-n,m=map(int,input().split())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
+a,b=map(int,input().split())
+l = list(map(int,input().split()))
+m = list(map(int,input().split()))
 c=0
-for i in sorted(set(a),key=a.index):
-    if b.count(i)!=0:
-        print(i,end=' ')
+for i in sorted(set(l),key=l.index):
+    if m.count(i)!=0:
+        print(i,end=" ")
